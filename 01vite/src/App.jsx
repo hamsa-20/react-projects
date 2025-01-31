4d666bdfab79bc8592dfb3ed8da7851515d939f6
hamsa <hamsadixit470@gmail.com>
2025-01-31 21:03:39 +0530

    Initial React setup with Vite

---
+++ 01vite/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai";
+
+
+
+function App() {
+  
+
+  return (
+    <>
+   <Chai></Chai>
+   <h1>hwllo im hamsaaaaa</h1>
+   <p>i am hungryyyy</p>
+   </>
+  );
+}
+
+export default App;
